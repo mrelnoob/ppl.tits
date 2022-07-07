42fc21fafd212ae25aa889f40921e5ea8e421c58
fanf <francois.martin74190@gmail.com>
2022-07-07 20:20:40 +0200

    :white_check_mark: Documented new function(s)

--- R/01_01_prepare_data.R
+++ R/01_01_prepare_data.R
@@ -5,18 +5,44 @@
 # The functions of this R file are meant to prepare datasets: e.g. aggregate observations,
 # delete or add variables, etc. But NOT to directly explore and clean data for analyses!
 
-# rtits <- ppl.tits::import_raw_tits_data()
-#
-#
-# # A function to simplify and aggregate raw-tits data:
-#
-# rtits %>% dplyr::select(-site, -nestling_mass_j14) %>%
-#   dplyr::filter(success != 'NA', age != 'NA') -> xxx
-#
-# summary(xxx)
 
 
+### ______________________________________
+#' Aggregate tits data by nestbox and year
+#'
+#' @description The `aggreg_by_nest` function aggregates the raw tits data to create a table
+#' summarizing tits' breeding success for each nestbox each year where breeding occurred. As such,
+#' it only keeps information on nestlings/juveniles and disregards the lines about adult tits.
+#'
+#' @return A tibble with 15 variables.
+#' @export
+#' @import dplyr
+#'
+#' @examples
+#' \dontrun{
+#' nestbox_repro <- aggreg_by_nest()
+#' }
+aggreg_by_nest <- function(){
+  rtits <- ppl.tits::import_raw_tits_data()
 
+  rtits %>% dplyr::filter(age == "nestling") %>%
+    dplyr::group_by(id_nestbox, year) %>%
+    dplyr::summarise(date = dplyr::first(date),
+                     laying_date = dplyr::first(laying_date),
+                     incubation_date = dplyr::first(incubation_date),
+                     hatching_date = dplyr::first(hatching_date),
+                     clutch_size = max(clutch_size),
+                     brood_size = max(brood_size),
+                     fledgling_nb = max(fledgling_nb),
+                     father_id = dplyr::first(father_id),
+                     mother_id = dplyr::first(mother_id),
+                     species = dplyr::first(species),
+                     mass = mean(nestling_mass_j13),
+                     tarsus_length = mean(nestling_tarsus_l),
+                     wing_length = mean(nestling_wing_l)) -> xxx
+
+  return(xxx)
+}
 
 
 
