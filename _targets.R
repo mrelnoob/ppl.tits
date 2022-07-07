dcfc41ceb9a870a61db0b2cbe88cdf9330a4ed0d
fanf <francois.martin74190@gmail.com>
2022-07-07 18:30:20 +0200

    :white_check_mark: Updated function(s)

--- _targets.R
+++ _targets.R
@@ -4,12 +4,12 @@
 
 #library(targets) # Perhaps I don't need it if i use ::!!!!!
 
-list(
-  # Make the workflow depends on the raw data file
-  targets::tar_target(raw_data_file, here::here("mydata", "ppl_dijon_tits_data_20192021.csv"),
-             format = "file"),
-  # Read the data and return a data.frame
-  targets::tar_target(raw_tits, ppl.tits::import_raw_tits_data())
-)
+# list(
+#   # Make the workflow depends on the raw data file
+#   targets::tar_target(raw_data_file, here::here("mydata", "ppl_dijon_tits_data_20192021.csv"),
+#              format = "file"),
+#   # Read the data and return a data.frame
+#   targets::tar_target(raw_tits, ppl.tits::import_raw_tits_data())
+# )
 # NOTE: it might not work because my function already calls here::here (I should perhaps change it first)!
 # But it seems to work!
