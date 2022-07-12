37fe0dd9b6baab3fc3b67db9c0c405458182d687
fanf <francois.martin74190@gmail.com>
2022-07-12 19:08:23 +0200

    :metal: Saved updates!

--- R/01_02_graphab_analyses.R
+++ R/01_02_graphab_analyses.R
@@ -14,20 +14,20 @@
 # field in the package description file. Thereby you formally declare that some additional piece of software
 # is needed to make the package work (https://www.r-bloggers.com/2021/09/how-to-use-system-commands-in-your-r-script-or-package/)!
 
-######################################### SUPER IMPORTANT NOTE #########################################
+######################################## SUPER IMPORTANT NOTE #########################################
 ### The EASIER way to start here will be to use this script ONLY to keep track of my command lines for
 # Graphab (so I should NOT try to load or install my package in the meantime UNLESS I assign this file
 # to .RBuildignore first !!!). When I'll have something that works smoothly, I will be able to translate
 # it to a true R script working as a CLI wrapper by using one of the packages described in the link above!
-###########################################################################################################
+# --------------------------------------------------------------------------------------------------- #
 
 
 
 
 
-# ------------------------- #
-##### 0. To begin #####
-# ------------------------- #
+# ----------------------- #
+##### 0. How to start #####
+# ----------------------- #
 
 # The first thing to do is to have the Graphab software (.jar) at the root of the project. I will
 # thus paste it there manually and tell R and Git to IGNORE it (because it's heavy and not expected)
@@ -35,6 +35,23 @@
 # of Graphab at the same place (here, Graphab 2.8)!
 usethis::use_build_ignore("graphab-2.8.jar")
 usethis::use_git_ignore("graphab-2.8.jar")
+
+
+
+# ---------------------------------------------------------------------------- #
+### * 0.1. Useful command lines (for Windows) ----------------------------------
+
+# ** 0.1.1. To know and change the current directory ----
+# cd # Or cd \ (+tab) for Windows
+# dir # For the content of the directory
+# rem # To comment (rem = remark). NOTE: this is a command, so it needs to be put first or after an operator
+# # if you comment AFTER a command (e.g. "cd & rem blabla"; and NOT "cd rem blabla"). So it sucks, that's
+# # because it is MS.DOS.
+# java -jar graphab-2.8.jar --help
+
+
+
+
 
 
 # ---------------------------------------------------------------------------- #
