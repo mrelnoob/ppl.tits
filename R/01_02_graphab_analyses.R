26707450e139fab589b38ede49eba893d5871ea8
fanf <francois.martin74190@gmail.com>
2022-07-12 16:26:24 +0200

    :boom: Started my Graphab script

--- R/01_02_graphab_analyses.R
+++ R/01_02_graphab_analyses.R
@@ -1,1 +1,20 @@
-# For all Graphab analyses?
+# -------------------------------------------------------- #
+##### Script (and functions?) for all Graphab analyses #####
+# -------------------------------------------------------- #
+
+# First, this will be my script for all Graphab analyses (preliminary and final). Then, if
+# my calls to the Terminal work, I'll consider create associated functions and targets.
+
+
+
+
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
+usethis::use_git(message = ":boom: Started my Graphab script")
+usethis::use_git(message = ":white_check_mark: Edited structure")
+usethis::use_git(message = ":metal: Saved updates!")
+#system("git push")
+# ---------------------------------------------------------------------------- #
+# ------------------------------- THE END ------------------------------------ #
+# ---------------------------------------------------------------------------- #
