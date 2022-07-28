7a36248bddfaebd322e8f8c99f473bd987513e2d
fanf <francois.martin74190@gmail.com>
2022-07-28 18:34:31 +0200

    :white_check_mark: Saved updates!

--- _targets.R
+++ _targets.R
@@ -9,7 +9,9 @@ list(
   targets::tar_target(raw_data_file, here::here("mydata", "ppl_dijon_tits_data.csv"),
              format = "file"),
   # Read the data and return a data.frame
-  targets::tar_target(raw_tits, ppl.tits::import_raw_tits_data())
-)
+  targets::tar_target(raw_tits, ppl.tits::import_raw_tits_data()),
+  # Export the nestling aggregated data table
+  targets::tar_target(nnestling_data, ppl.tits::export_nestling_aggreg(), format = file)
+  )
 # NOTE: it might not work because my function already calls here::here (I should perhaps change it first)!
 # But it seems to work!
