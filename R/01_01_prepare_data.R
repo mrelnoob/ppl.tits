7ccfcdc1e720beb4a9f706533964dca8fa3fce47
fanf <francois.martin74190@gmail.com>
2022-07-28 18:13:12 +0200

    :metal: Created the export nestling data function!

--- R/01_01_prepare_data.R
+++ R/01_01_prepare_data.R
@@ -49,40 +49,25 @@ aggreg_by_nest <- function(){
   return(xxx)
 }
 
-#### NOW i should targets it (or not yet), and export it (and then target it???)!!!
 
 
 
 
-
-
-
-# tits %>% dplyr::filter(species == "CC") -> CC
-# tits %>% dplyr::filter(species == "PM") -> PM
-# summary(CC)
-# summary(PM)
-#
-# CC %>% dplyr::filter(age != "nestling" & age != "NA") -> adult_cc
-# PM %>% dplyr::filter(age != "nestling" & age != "NA") -> adult_pm
-# summary(adult_cc)
-# summary(adult_pm)
-#
-#
-#
-# ### Number of nestboxes per...
-# # Species:
-# CC %>% group_by(id_nestbox) %>%
-#   summarize(nb_obs = n()) # So 85 nestboxes with data for the 3 years (23 of which have data for several years)
-# PM %>% group_by(id_nestbox) %>%
-#   summarize(nb_obs = n()) # So 132 nestboxes with data for the 3 years (41 of which have data for several years)
-# adult_pm %>% group_by(id_nestbox) %>%
-#   summarize(nb_obs = n()) # So 93 nestboxes with adult data for the 3 years (41 of which have data for several years)
-#
-# # Year:
-# CC %>% group_by(year, id_nestbox) %>%
-#   summarize(nb_obs = n()) -> cc_eff_year_box
-# PM %>% group_by(year, id_nestbox) %>%
-#   summarize(nb_obs = n()) -> pm_eff_year_box
-# adult_pm %>% group_by(year, id_nestbox) %>%
-#   summarize(nb_obs = n()) -> adultpm_eff_year_box
-
+### ______________________________________
+#' Export tits nestling aggregated dataset
+#'
+#' @description The `export_nestling_aggreg` function exports the dataset (.csv) created by the
+#' `aggreg_by_nest` function, that is the tits nestling data aggregated by nestbox and year.
+#' @return A CSV table (stored in output/tables).
+#' @export
+#' @importFrom readr write_csv2
+#' @importFrom here here
+#' #'
+#' @examples
+#' \dontrun{
+#' export_nestling_aggreg()
+#' }
+export_nestling_aggreg <- function(){
+  aabb <- ppl.tits::aggreg_by_nest()
+  readr::write_csv2(x = aabb, file = here::here("output", "tables", "tits_nestling_data.csv"))
+}
