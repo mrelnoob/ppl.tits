7cbdfe032d111b6e565c2d91b70e40685d71cf11
fanf <francois.martin74190@gmail.com>
2022-07-29 11:39:11 +0200

    :white_check_mark: Saved updates!

--- R/01_01_prepare_data.R
+++ R/01_01_prepare_data.R
@@ -72,4 +72,5 @@ aggreg_by_nest <- function(){
 export_nestling_aggreg <- function(){
   aabb <- ppl.tits::aggreg_by_nest()
   readr::write_csv2(x = aabb, file = here::here("output", "tables", "tits_nestling_data.csv"))
+  return(paste("Table exported to", here::here("output", "table"), sep = " "))
 }
