663ec5846fc32c982b92d524bd282c580ee26dcd
Fanfoué <francois.martin74190@gmail.com>
2022-08-08 18:25:30 +0200

    :x: Update on my problem!

--- _targets.R
+++ _targets.R
@@ -15,3 +15,8 @@ list(
   )
 ### La dernière target "marche", mais ne génère que le path (et pas la table), et il n'y a pas de connexions
 # entre mes targets dans la visualisation. Pk???
+# C'est sans doute car je n'appelle pas les targets créés dans mes fonctions, donc targets n'arrive pas à suivre réellement
+# les fichiers et modifs si les fonctions utilisées comme arguments des tar_target() n'appelle pas les objets targets déjà créés ??
+# Il faut que je lise "https://books.ropensci.org/targets/" si je veux vraiment m'y mettre, car ça ne va pas. Quand j'aurais le
+# temps. Voir aussi : https://stackoverflow.com/questions/68683153/preferred-approach-for-making-the-targets-r-package-detect-changes-to-functi
+# qui traite du développement de fonctions/packages en utilisant TRAGETS§§§
