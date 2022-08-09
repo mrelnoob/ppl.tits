54fb183d54243eb38ce40713d483b88b6bdd5b9a
Fanfoué <francois.martin74190@gmail.com>
2022-08-09 10:32:12 +0200

    :x: Update on my problem

--- _targets.R
+++ _targets.R
@@ -2,7 +2,9 @@
 ##### Structure of my data processing workflow #####
 # ------------------------------------------------ #
 
-#library(targets) # Perhaps I don't need it if i use ::!!!!!
+#library(targets) # I don't need it if i use "::"!
+# I should also source my custom functions but I don't want to, because that's one of the advantages of
+# building an R package.
 
 list(
   # Make the workflow depends on the raw data file
@@ -13,8 +15,10 @@ list(
   # Export the nestling aggregated data table
   targets::tar_target(nnestling_data, ppl.tits::export_nestling_aggreg(), format = "file")
   )
-### La dernière target "marche", mais ne génère que le path (et pas la table), et il n'y a pas de connexions
-# entre mes targets dans la visualisation. Pk???
+### The last 'target' works (so to speak) but does not export the table: it only returns the path. Also, there are no
+# connections between my 'targets' in the visualisation (because I do not call previous 'targets' in my 'targets' so
+# it cannot keep track???)....
+# I lack time for this...
 # C'est sans doute car je n'appelle pas les targets créés dans mes fonctions, donc targets n'arrive pas à suivre réellement
 # les fichiers et modifs si les fonctions utilisées comme arguments des tar_target() n'appelle pas les objets targets déjà créés ??
 # Il faut que je lise "https://books.ropensci.org/targets/" si je veux vraiment m'y mettre, car ça ne va pas. Quand j'aurais le
