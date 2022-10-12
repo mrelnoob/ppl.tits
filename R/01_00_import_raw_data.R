534f41392809cd00ab5a6f63798db8615a22f4ae
fanf <francois.martin74190@gmail.com>
2022-10-12 16:52:00 +0200

    :white_check_mark: Corrected import function to patch bug

--- R/01_00_import_raw_data.R
+++ R/01_00_import_raw_data.R
@@ -21,6 +21,8 @@
 #' @importFrom here here
 #' @importFrom dplyr select
 #' @importFrom dplyr filter
+#' @importFrom dplyr case_when
+#' @importFrom dplyr mutate
 #'
 #' @examples
 #' \dontrun{
@@ -74,7 +76,7 @@ import_raw_tits_data <- function(mypath = here::here("mydata", "ppl_dijon_tits_d
   # variable, I have to first convert the variable as a character one!
   xxx[xxx$id_bird == "BRFAIPM1120" &
         xxx$id_ring == "8877045", "id_bird"] <- "THPAGPM095X"
-  xxx$id_bird <- as.factor(xxx$id_bird)
+  xxx$id_bird <- as.factor(xxx$id_bird) # And I convert it back into a factor.
 
   # TO correct the 'laying_date' of the female tits with 'id_bird' = CLPECPM0005:
   xxx$laying_date <- as.character(xxx$laying_date) # Idem.
@@ -83,25 +85,58 @@ import_raw_tits_data <- function(mypath = here::here("mydata", "ppl_dijon_tits_d
 
   # TO correct wrong father_id values for DIJ-272_2021 and DIJ-134_2022:
   xxx$father_id <- as.character(xxx$father_id) # Idem.
-  xxx[xxx$father_id == "8876210" &
-        xxx$year == "2021", "father_id"] <- "8876510"
-
-  xxx[xxx$father_id == "8877594" &
-        xxx$year == "2022", "father_id"] <- "8876594"
+  xxx %>%
+    dplyr::mutate(
+      father_id = dplyr::case_when(
+        father_id != "8876210" | year != "2021" ~ father_id, # Here, I have to say that "everything
+        # that is not my selection stays as is"! If I don't (and only run the next line), I will
+        # get NA for all other observations. NOTE also that as this is a "negative selection"
+        # (using the != operator), I need to reverse the "&" as well, however illogical it may be.
+        father_id == "8876210" & year == "2021" ~ "8876510")) %>%
+    dplyr::mutate( # Note also that I have to run each modification as a separate "mutate"!
+      father_id = dplyr::case_when(
+        father_id != "8877594" | year != "2022" ~ father_id,
+        father_id == "8877594" & year == "2022" ~ "8876594")) -> xxx
+  # NOTE: Here, I changed my way of correcting mistakes because there is a bug somewhere in R
+  # preventing me from doing it as I used to. My former code was:
+  # xxx$father_id <- as.character(xxx$father_id) # Idem.
+  # xxx[xxx$father_id == "8876210" &
+  #       xxx$year == "2021", "father_id"] <- "8876510"
+  # xxx[xxx$father_id == "8877594" &
+  #       xxx$year == "2022", "father_id"] <- "8876594"
+  # And it used to work fine. But, at some point (weeks after I wrote these lines), it stopped
+  # working and generated an error saying: "Can't use NA as row index in a tibble for assignment".
+  # It probably has something to do with an update somewhere and a bug between R and Windows. It
+  # doesn't make sense otherwise. The problem is linked to how R handles NA in factors but it
+  # TRULY doesn't make sense (but I don't have time to report it properly), but trust me!
   xxx$father_id <- as.factor(xxx$father_id)
 
   # TO correct wrong id_ring values for the females of DIJ-133_2022 and DIJ-179_2020:
-  xxx$id_ring <- as.character(xxx$id_ring) # Idem.
-  xxx[xxx$id_ring == "8877950" &
-        xxx$year == "2022", "id_ring"] <- "8877590"
-
-  xxx[xxx$id_ring == "8381599" &
-        xxx$year == "2020", "id_ring"] <- "8581599"
-  xxx$id_ring <- as.factor(xxx$id_ring)
+  xxx$id_ring <- as.character(xxx$id_ring) # Idem and same problem here!
+  xxx %>%
+    dplyr::mutate(
+      id_ring = dplyr::case_when(
+        id_ring != "8877950" | is.na(id_ring) == "TRUE" ~ id_ring,
+        id_ring == "8877950" & year == "2022" ~ "8877590")) %>%
+    dplyr::mutate(
+      id_ring = dplyr::case_when(
+        id_ring != "8381599" | is.na(id_ring) == "TRUE" ~ id_ring,
+        id_ring == "8381599" & year == "2020" ~ "8581599")) -> xxx
+  # NOTE: here, there is another bug as it doesn't work unless I specify observations without NA
+  # while the exact same procedure work above for "father_id" which contains NA as well...
+  xxx$id_ring <- as.factor(xxx$id_ring) # Idem and same problem here!
 
   # To correct the gender/sex of the presumed father of DIJ-212_2022:
-  xxx[xxx$id_ring == "8877107", "sex"] <- "male" # Here, as i'm not trying to add a new level to
-  # the 'sex' factor, I do not need to convert 'the 'sex' into a character variable.
+  xxx$sex <- as.character(xxx$sex) # Idem and same problem here!
+    xxx %>%
+    dplyr::mutate(
+      sex = dplyr::case_when(
+        id_ring != "8877107" | is.na(id_ring) == "TRUE" ~ sex,
+        id_ring == "8877107" ~ "male")) -> xxx
+
+  xxx %>% dplyr::filter(id_ring == "8877107") -> a
+  xxx %>% dplyr::filter(id_ring != "8877107" | is.na(id_ring) == "TRUE")
+  xxx$sex <- as.factor(xxx$sex)
 
   # To delete late reproduction events (i.e. for DIJ-175 in 2019 and DIJ-044 in 2022):
   xxx %>% dplyr::filter(date != "28/06/2022") %>%
@@ -112,12 +147,11 @@ import_raw_tits_data <- function(mypath = here::here("mydata", "ppl_dijon_tits_d
     adult_aggress = as.numeric(gsub(x = adult_aggress, pattern = ",", replacement = "."))) -> xxx # As
   # "adult_aggress" contained commas instead of decimal points, I had to use the 'gsub()' function to
   # convert them (it's a base function close to 'grep()').
-  summary(xxx)
 
 
 
   ### To avoid warnings during the R CMD check__________________________________#
-  site <- success <- date <- laying_date <- adult_aggress <- age <- NULL
+  site <- success <- date <- laying_date <- adult_aggress <- age <- id_ring <- sex <- year <- NULL
   # This is necessary for all not globally
   # defined variables, that is variables that are called without "$" (such as calls from
   # {dplyr}; e.g. if your function uses something like dplyr::filter(myvar == "something"),
