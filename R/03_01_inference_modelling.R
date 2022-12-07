303a660e0d1c4f4bc12f9ca263a36f011719b865
Fanfoué <francois.martin74190@gmail.com>
2022-12-07 16:44:10 +0100

    :hammer: Ongoing programming!

--- R/03_01_inference_modelling.R
+++ R/03_01_inference_modelling.R
@@ -17,11 +17,60 @@
 # SHOULD START BY CALLING/SOURCING the EDA script first (because no function yet???)!
 
 ##### PM MODELS #####
+
+pm %>% dplyr::mutate(f_metric = scale(pmF_d531_beta0),
+                     woodvol = scale(woodyveg_vw),
+                     urbanity = scale(urban_intensity),
+                     light_pol = scale(light_pollution),
+                     noise_pol = scale(noise_m),
+                     temperature = scale(cumdd_30),
+                     male_cond = scale(father_cond),
+                     female_cond = scale(mother_cond),
+                     f_metric_cent = scale(pmF_d531_beta0, scale = FALSE),
+                     woodvol_cent = scale(woodyveg_vw, scale = FALSE)) -> pm2
+## QUID de factors????? manag_intensity + year (et quid des levels de year?)!!!
+# schielzeth dit dummy coding + centrage -----> mais ça dépend des objectifs!!!
+# Sans dummy: manag0 = gestion nat; year0 = 2019!
+# Mais poser question CV car toujours pas vraiment régler cette histoire de scaling/centrage!!!!!!
+
 ##### 1. Clutch size: Poisson GLMM #####
 # ______________________________________
 
+### Initial model fit___________________________________________________________#
+# Without centering or scaling:
+pmclutch_glmm0 <- lme4::glmer(clutch_size ~ woodyveg_vw*pmF_d531_beta0 + urban_intensity +
+                                manag_intensity + light_pollution + noise_m + cumdd_30 + father_cond +
+                                mother_cond + (1|id_nestbox) + (1|breeding_window),
+                              data = pm2, family = poisson) #### Voir ?glmerControl
+# Voir rescaling (jouer avec les unités) > std > glmercontrol!!!!!!!!
+# Voir rescaling (jouer avec les unités) > std > glmercontrol!!!!!!!!
+# Voir rescaling (jouer avec les unités) > std > glmercontrol!!!!!!!!
+# Voir rescaling (jouer avec les unités) > std > glmercontrol!!!!!!!!
+# Voir rescaling (jouer avec les unités) > std > glmercontrol!!!!!!!!
+# Voir rescaling (jouer avec les unités) > std > glmercontrol!!!!!!!!
+
+
+### Checking preliminary assumptions____________________________________________#
+# Poisson distribution:
+theo_count <- rpois(n = nrow(pm), lambda = mean(pm$clutch_size))
+tc_df <-data.frame(theo_count)
+
+ggplot2::ggplot(pm, ggplot2::aes(clutch_size)) +
+  ggplot2::geom_bar(fill = "#1E90FF") +
+  ggplot2::geom_bar(data = tc_df, ggplot2::aes(theo_count, fill="#1E90FF", alpha=0.5)) +
+  ggplot2::theme_classic() +
+  ggplot2::theme(legend.position = "none") # Blue = observed counts; red = simulated.
+# This plot suggests that clutch_size may not be following a Poisson distribution (underdispersion?
+# Normality?), but perhaps it's ok.
+
+# Overdispersion:
+aods3::gof()
+
+
+
+
 
 hist(pm$clutch_size)
-
-colnames(pm)
+ppl.tits::uni.histograms(pm2)
+colnames(pm2)
 summary(pm)
